59b2fff9b4c467d6863974bbb8b694e3ed7a2034
rKaushalya <ravinduk56@gmail.com>
2023-07-21 00:48:02 +0530

    Idle Animation Created

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,17 @@
-function idleNinja() {
+var ninja = document.getElementById("ninja");
+let idleImgNumber = 0;
+let idleAnimationNumber = 0;
 
+function idleNinja() {
+    idleImgNumber ++;
+
+    if (idleImgNumber == 11){
+        idleImgNumber = 1;
+    }
+
+    ninja.src = "asset/img/sprites/Idle__"+idleImgNumber+".png";
+}
+
+function idleAnimationStart() {
+    idleAnimationNumber = setInterval(idleNinja, 200);
 }
